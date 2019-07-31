3be283cecfa7dca80db80ec56ba7e932a6d9a6c4
Henry Tran <henrytrann28@gmail.com>
2019-07-30 19:03:26 -0600

    experimented with saving users in firestore

--- src/components/firebase/firebase.js
+++ src/components/firebase/firebase.js
@@ -1,19 +1,36 @@
 import config from './config';
 import app from 'firebase/app';
+import 'firebase/firestore';
 import 'firebase/auth';
 
 class Firebase {
   constructor() {
     app.initializeApp(config);
     this.auth = app.auth();
+    this.db = app.firestore();
   }
 
-   signInPopup = (provider) => {
+   signInPopup = async (provider) => {
     this.auth.signInWithPopup(provider)
   }
+      // this.props.firebase.db.collection("users").add(dbuser);
 
   signOut = () => {
     this.auth.signOut();
+  }
+
+  isExistingUser = async (email) => {
+    let results = await this.db.collection("users").where("email", "==", email).get();
+    console.log(results.size)
+    return results.size !== 0;
+  }
+
+  addUser = async (user) => {
+      let dbuser = {
+        name: user.displayName,
+        email: user.email
+      };
+      await this.db.collection("users").add(dbuser);
   }
 }
 
