4f7229cd3665a1c5bec1ba47f640ef213b00ba9c
Tai Lai <ctlai95@gmail.com>
2019-08-02 22:49:24 -0600

    Add unit tests for actions

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   "presets": ["@babel/env", "@babel/react"],
-  "plugins": ["@babel/plugin-proposal-class-properties"],
+  "plugins": ["@babel/plugin-proposal-class-properties", "@babel/plugin-syntax-export-default-from"],
 }
