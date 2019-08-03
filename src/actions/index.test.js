02fdbb56e2362803766f0627de83a410eb45ca57
Tai Lai <ctlai95@gmail.com>
2019-08-03 16:01:26 -0600

    Add tests for action creators

--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -4,7 +4,6 @@ import * as types from "./types";
 describe("actions", () => {
   it("should create an action to update auth state", () => {
     const authState = {
-      type: types.UPDATE_AUTH_STATE,
       loggedIn: true
     };
     const expectedAction = {
@@ -12,5 +11,21 @@ describe("actions", () => {
       authState
     };
     expect(actions.updateAuthStateAction(authState)).toEqual(expectedAction);
+  });
+
+  it("should create an action to add a new note", () => {
+    const expectedAction = {
+      type: types.ADD_NEW_NOTE
+    };
+    expect(actions.addNewNoteAction()).toEqual(expectedAction);
+  });
+
+  it("should create an action to set a note active", () => {
+    let noteId = 0;
+    const expectedAction = {
+      type: types.SET_NOTE_ACTIVE,
+      noteId
+    };
+    expect(actions.setNoteActiveAction(noteId)).toEqual(expectedAction);
   });
 });
