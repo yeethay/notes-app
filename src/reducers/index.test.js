e6971f376bee1101b1df06539238310bd7ab2805
Tai Lai <ctlai95@gmail.com>
2019-08-03 15:50:01 -0600

    Add tests for reducer

--- src/reducers/index.test.js
+++ src/reducers/index.test.js
@@ -1,28 +1,119 @@
 import rootReducer from ".";
 import * as types from "../actions/types";
+import * as actions from "../actions";
+import initialValue from "../components/editor/initialValue";
+import { Value, Document } from "slate";
 
 describe("root reducer", () => {
   it("should return the initial state", () => {
-    expect(rootReducer(undefined, {})).toEqual({
+    let initialState = {
       loggedIn: undefined,
       notesList: [],
       currentNoteIndex: 0
-    });
+    };
+    expect(rootReducer(undefined, {})).toEqual(initialState);
   });
 
   it("should update auth state", () => {
+    let initialState = {
+      loggedIn: undefined,
+      notesList: [],
+      currentNoteIndex: 0
+    };
     expect(
-      rootReducer(undefined, {
-        type: types.UPDATE_AUTH_STATE,
-        authState: { loggedIn: true }
-      })
-    ).toEqual({ loggedIn: true, notesList: [], currentNoteIndex: 0 });
+      rootReducer(
+        initialState,
+        actions.updateAuthStateAction({ loggedIn: true })
+      )
+    ).toEqual({ ...initialState, loggedIn: true });
 
     expect(
-      rootReducer(undefined, {
-        type: types.UPDATE_AUTH_STATE,
-        authState: { loggedIn: false }
-      })
-    ).toEqual({ loggedIn: false, notesList: [], currentNoteIndex: 0 });
+      rootReducer(
+        initialState,
+        actions.updateAuthStateAction({ loggedIn: false })
+      )
+    ).toEqual({ ...initialState, loggedIn: false });
+  });
+
+  it("should add a new note when none exist", () => {
+    let initialState = {
+      loggedIn: undefined,
+      notesList: [],
+      currentNoteIndex: 0
+    };
+    let newState = rootReducer(initialState, actions.addNewNoteAction());
+
+    expect(newState.notesList.length).toEqual(1);
+    expect(newState.notesList[0]).toEqual({
+      title: "",
+      preview: "",
+      value: initialValue,
+      active: true,
+      id: 0
+    });
+  });
+
+  it("should add a new note when there is already an existing one", () => {
+    let initialState = {
+      notesList: [
+        {
+          title: "",
+          preview: "",
+          value: initialValue,
+          active: true,
+          id: 0
+        }
+      ]
+    };
+    let newState = rootReducer(initialState, actions.addNewNoteAction());
+
+    expect(newState.notesList.length).toEqual(2);
+    expect(newState.notesList).toEqual([
+      {
+        title: "",
+        preview: "",
+        value: initialValue,
+        active: false,
+        id: 0
+      },
+      {
+        title: "",
+        preview: "",
+        value: initialValue,
+        active: true,
+        id: 1
+      }
+    ]);
+  });
+
+  it("should set a note active", () => {
+    let initialState = {
+      notesList: [{ active: true, id: 0 }, { active: false, id: 1 }],
+      currentNoteIndex: 0
+    };
+
+    let expectedState = {
+      notesList: [{ active: false, id: 0 }, { active: true, id: 1 }],
+      currentNoteIndex: 1
+    };
+    expect(rootReducer(initialState, actions.setNoteActiveAction(1))).toEqual(
+      expectedState
+    );
+  });
+
+  it("should change the title of a note", () => {
+    let initialState = {
+      notesList: [{ title: "Before", id: 0 }],
+      currentNoteIndex: 0
+    };
+
+    let expectedState = {
+      notesList: [{ title: "After", id: 0 }],
+      currentNoteIndex: 0
+    };
+
+    expect(
+      rootReducer(initialState, actions.setNoteTitleAction("After"))
+    ).toEqual(expectedState);
   });
 });
