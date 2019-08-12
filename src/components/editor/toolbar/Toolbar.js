101c089f8e294db365d05346b5ef49c4611d1b34
Tai Lai <ctlai95@gmail.com>
2019-08-11 22:32:41 -0600

    Add visual indicator for active slate buttons

--- src/components/editor/toolbar/Toolbar.js
+++ src/components/editor/toolbar/Toolbar.js
@@ -1,27 +1,8 @@
 import React, { Component } from "react";
-import Icon from "react-icons-kit";
 
 class Toolbar extends Component {
-  getButtonsList() {
-    let buttons = [];
-
-    for (let key in this.props.buttons) {
-      buttons.push(
-        <button
-          key={key}
-          onPointerDown={this.props.buttons[key].onPointerDown}
-          className="tooltip-icon-button"
-        >
-          <Icon icon={this.props.buttons[key].icon} />
-        </button>
-      );
-    }
-
-    return buttons;
-  }
-
   render() {
-    return <div className="format-toolbar">{this.getButtonsList()}</div>;
+    return <div className="format-toolbar" {...this.props} />;
   }
 }
 
