68d708081105f11248ff89438243330fef9d15b3
henrytran28 <henrytrann28@gmail.com>
2019-10-14 14:43:37 -0600

    added saving titles to componentdidmount

--- src/components/editor/Title.js
+++ src/components/editor/Title.js
@@ -1,38 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { setNoteTitleAction } from '../../actions';
 import { withFirebase } from '../firebase';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import './styles/Title.css';
 
-const Title = props => {
-  return (
-    <textarea
-      maxLength={50}
-      value={props.titlesList[props.currentNoteIndex].title}
-      onChange={(event) => onChange(props, event)}
-      onKeyDown={onKeyDown}
-      placeholder="Untitled note"
-    />
-  );
-};
+class Title extends Component {
+  componentDidMount() {
+    let user = this.props.firebase.auth.currentUser;
+    if (!user)
+      return;
 
-const onChange = (props, event) => {
-  let newValue = event.target.value;
-  props.dispatch(setNoteTitleAction(newValue));
+    console.log('HELLO');
 
-  let user = props.firebase.auth.currentUser;
-  if (!user)
-    return;
+    this.props.firebase.saveTitlesToDB(user, this.props.titlesList);
+  };
 
-  props.firebase.saveTitlesToDB(user, props.titlesList);
-};
+  onChange = event => {
+    let newValue = event.target.value;
+    this.props.dispatch(setNoteTitleAction(newValue));
 
-const onKeyDown = event => {
-  if (event.keyCode === 13) {
-    event.preventDefault();
-  }
-};
+    let user = this.props.firebase.auth.currentUser;
+    if (!user)
+      return;
+
+    this.props.firebase.saveTitlesToDB(user, this.props.titlesList);
+  };
+
+  onKeyDown = event => {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+    }
+  };
+
+  render() {
+    return (
+      <textarea
+        maxLength={50}
+        value={this.props.titlesList[this.props.currentNoteIndex].title}
+        onChange={(event) => this.onChange(event)}
+        onKeyDown={this.onKeyDown}
+        placeholder="Untitled note"
+      />
+    );
+  };
+}
 
 Title.propTypes = {
   text: PropTypes.node,
