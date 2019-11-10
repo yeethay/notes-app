ea6b0e89be21ef9498eadad4d6b5a83d7fa81a6f
Tai Lai <ctlai95@gmail.com>
2019-11-09 22:44:31 -0700

    Run saga middleware

--- src/store/index.js
+++ src/store/index.js
@@ -8,6 +8,6 @@ const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(rootReducer, applyMiddleware(logger, sagaMiddleware));
 
-// sagaMiddleware.run(rootSaga);
+sagaMiddleware.run(rootSaga);
 
 export default store;
