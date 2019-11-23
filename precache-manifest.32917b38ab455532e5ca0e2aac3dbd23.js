45a96802dee36154a045370cc989921792fcc8ac
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-23 23:32:29 +0000

    Deploy fouronesixnine/notes-app to github.com/fouronesixnine/notes-app.git:gh-pages

---
+++ precache-manifest.32917b38ab455532e5ca0e2aac3dbd23.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50770fced618d70ecdf79eb1841b51e1",
+    "url": "/notes-app/index.html"
+  },
+  {
+    "revision": "96e4012de1d8914c1d28",
+    "url": "/notes-app/static/css/main.742c3ff8.chunk.css"
+  },
+  {
+    "revision": "24938147840d1414b403",
+    "url": "/notes-app/static/js/2.8750f334.chunk.js"
+  },
+  {
+    "revision": "96e4012de1d8914c1d28",
+    "url": "/notes-app/static/js/main.b5a90dc9.chunk.js"
+  },
+  {
+    "revision": "6e2588e379dbc4286e82",
+    "url": "/notes-app/static/js/runtime~main.2c46edbd.js"
+  }
+]);
