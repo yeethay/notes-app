30570a2918951dda6dc8732d0acb29eef560ad59
Tai Lai <ctlai95@gmail.com>
2019-11-22 18:05:16 -0700

    Add firebase config

---
+++ src/sagas/firebaseConfig.tsx
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: 'AIzaSyDqk-tCkJUNKqN1rwhHDvOUrMQ80KDdnYI',
+  authDomain: 'fouronesixnine-notes-app.firebaseapp.com',
+  databaseURL: 'https://fouronesixnine-notes-app.firebaseio.com',
+  projectId: 'fouronesixnine-notes-app',
+  storageBucket: '',
+  messagingSenderId: '967381980154',
+  appId: '1:967381980154:web:5a5a8276ddab77a1',
+};
+
+export default firebaseConfig;
